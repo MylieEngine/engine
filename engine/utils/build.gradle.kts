18fc6a93f348c2f3b985d47410e0bf846d4b0e2b
Michael Zuegg <zzuegg@users.noreply.github.com>
2025-02-17 21:54:39 +0100

    Add Mockito dependency and configure Java agent for tests
    Introduce Mockito library for testing and set up a dedicated configuration for its Java agent. Adjust test task to include the agent via JVM arguments, enhancing mocking capabilities in unit tests.

--- engine/utils/build.gradle.kts
+++ engine/utils/build.gradle.kts
@@ -2,12 +2,20 @@ plugins {
     id("java-library")
 }
 
+val mockitoAgent = configurations.create("mockitoAgent")
+
 dependencies {
+    implementation(libs.logging.runtime)
     testImplementation(platform("org.junit:junit-bom:5.11.4"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation(libs.mockito)
+    mockitoAgent(libs.mockito) { isTransitive = false }
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-tasks.named<Test>("test") {
-    useJUnitPlatform()
+tasks{
+    test{
+        useJUnitPlatform()
+        jvmArgs("-Xshare:off","-javaagent:${mockitoAgent.asPath}")
+    }
 }
