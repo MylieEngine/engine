c3f96cd845ffec233becb826a69df4758a4a5c71
Michael Zuegg <zzuegg@users.noreply.github.com>
2025-02-17 20:47:50 +0100

    Add build configuration for consistent testing and formatting
    Introduce `build.gradle.kts` in `engine/utils` with JUnit setup. Enhance subproject configurations with Spotless, JaCoCo, and dependency management. Optimize build performance by enabling Gradle daemon and parallel execution.

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,3 +47,50 @@ dependencies {
         jacocoAggregation(project(it.path))
     }
 }
+
+subprojects {
+    group = rootProject.group
+    version = rootProject.version
+    repositories {
+        mavenCentral()
+    }
+
+    afterEvaluate {
+        if(project.hasProperty("java-library")||project.hasProperty("java")){
+            apply(plugin = libs.plugins.spotless.get().pluginId)
+            apply(plugin = "jacoco")
+
+            tasks.withType(JacocoReport::class.java).all {
+                dependsOn(tasks.findByName("test"))
+                reports {
+                    xml.required.set(true)
+                }
+            }
+
+            tasks.withType<Test>().configureEach {
+                finalizedBy(tasks.withType<JacocoReport>())
+            }
+
+            dependencies {
+                val implementation by configurations
+                val compileOnly by configurations
+                val annotationProcessor by configurations
+                val api by configurations
+                api(libs.logging.api)
+                compileOnly(libs.lombok)
+                annotationProcessor(libs.lombok)
+            }
+
+            spotless{
+                java{
+                    removeUnusedImports()
+                    importOrder()
+                    eclipse().configFile(rootProject.file("javaFormat.xml"))
+                    formatAnnotations()
+                    trimTrailingWhitespace()
+                    endWithNewline()
+                }
+            }
+        }
+    }
+}
