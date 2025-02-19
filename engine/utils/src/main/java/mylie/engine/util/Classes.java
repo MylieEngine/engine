8104616149975c5e945c04365d6cb51e721d68b7
Michael Zuegg <zzuegg@users.noreply.github.com>
2025-02-19 22:27:08 +0100

    Add utility class for safe object instantiation
    Introduce `Classes` utility with a generic `newInstance` method to safely create object instances using reflection. Logs errors and throws a meaningful exception when instantiation fails.

---
+++ engine/utils/src/main/java/mylie/engine/util/Classes.java
@@ -0,0 +1,17 @@
+package mylie.engine.util;
+
+import java.lang.reflect.InvocationTargetException;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Classes {
+	public static <T> T newInstance(Class<T> clazz) {
+		try {
+			return clazz.getDeclaredConstructor().newInstance();
+		} catch (InstantiationException | IllegalAccessException | InvocationTargetException
+				| NoSuchMethodException e) {
+			log.error("Failed to instantiate class: {}", clazz.getName(), e);
+			throw new IllegalStateException("Failed to instantiate class: " + clazz.getName(), e);
+		}
+	}
+}
