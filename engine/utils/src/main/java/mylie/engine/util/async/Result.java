7d523a25834a3751cc7629b910600f38f0053d10
Michael Zuegg <zzuegg@users.noreply.github.com>
2025-02-19 12:54:19 +0100

    Add execution control and result handling to Result class
    Introduced new fields and methods to manage the execution state and retrieve results effectively. The changes ensure tasks run only once, leveraging an AtomicBoolean for thread safety, and added logic to execute functions conditionally based on the target. This improves concurrency handling and aligns execution with the specified target type.

--- engine/utils/src/main/java/mylie/engine/util/async/Result.java
+++ engine/utils/src/main/java/mylie/engine/util/async/Result.java
@@ -1,16 +1,36 @@
 package mylie.engine.util.async;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Supplier;
 import lombok.AccessLevel;
 import lombok.Getter;
 @Getter(AccessLevel.PACKAGE)
 public class Result<R> {
 	final Hash hash;
 	final long version;
+	final Target target;
+	final Supplier<R> function;
 	final CompletableFuture<R> future;
-	Result(Hash hash, long version) {
+	final AtomicBoolean running = new AtomicBoolean(false);
+	Result(Hash hash, long version, Target target, Supplier<R> function) {
 		this.hash = hash;
 		this.version = version;
+		this.target = target;
+		this.function = function;
 		this.future = new CompletableFuture<>();
+	}
+
+	public boolean completed() {
+		return future.isDone();
+	}
+
+	public R result() {
+		if (!completed() && (target == Target.BACKGROUND || target.isCurrent())) {
+			if (running.compareAndSet(false, true)) {
+				AsyncUtil.checkedExecute(function, this);
+			}
+		}
+		return future.join();
 	}
 }
