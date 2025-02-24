d9eed03661e09d5a8d382349b0c24bd080152c4f
Michael Zuegg <zzuegg@users.noreply.github.com>
2025-02-24 19:26:51 +0100

    Add unit tests for Blocking utility class
    Introduce tests to validate the Blocking class behaviors, including its private constructor restriction and handling of interrupted blocking operations. Ensures higher reliability and compliance with expected utility class conventions.

---
+++ engine/utils/src/test/java/mylie/engine/util/BlockingTest.java
@@ -0,0 +1,41 @@
+package mylie.engine.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.lang.reflect.Constructor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class BlockingTest {
+
+	@Test
+	void testCreateInstance() {
+		Constructor<?> declaredConstructor = Blocking.class.getDeclaredConstructors()[0];
+		declaredConstructor.setAccessible(true);
+		Exception e = Assertions.assertThrows(Exception.class, declaredConstructor::newInstance);
+		Assertions.assertEquals(IllegalStateException.class, e.getCause().getClass());
+		Assertions.assertEquals("Utility class", e.getCause().getMessage());
+	}
+
+	@Test
+	void testBlockingInterrupt() {
+		Thread thread = Thread.currentThread();
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					throw new RuntimeException(e);
+				}
+				thread.interrupt();
+			}
+		}).start();
+		LinkedBlockingQueue<Object> objects = new LinkedBlockingQueue<>();
+		Assertions.assertThrows(IllegalStateException.class, () -> Blocking.poll(objects, 1, TimeUnit.SECONDS));
+	}
+
+}
